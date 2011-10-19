c90a8646dedd336174b0404283e4ca537b8077c4
Mat Malone <mmalone@datacare.com>
2011-10-19 17:32:06 +0000

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+<title>i n n e r l o g i c . o r g</title>
+</head>
+<body bgcolor="black" text="#d8d0b5">
+<center>
+<br><br><br>
+<img src=./head.jpg border=0>
+<br><br><br>
+
+<?php
+
+#$msgs = array("We'll try being nicer if you'll try being smarter.", "Eagles may soar, but weasels don't get sucked into jet engines.","Yes, we're agents of Satan, but our duties are largely ceremonial.", "All you need in this life is ignorance and confidence; then success is sure.");
+
+#echo '"' . $msgs[rand(0,count($msgs)-1)] . '"';
+
+?>
+
+</center>
+</body>
+</html>
+
+
